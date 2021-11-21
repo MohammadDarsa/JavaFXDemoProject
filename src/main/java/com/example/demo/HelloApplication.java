83e61ae6c4c48341b2930dfc7dc81572395fc6c8
Darsa <darsafxz@gmail.com>
2021-11-21 19:44:32 +0200

    javafx stages

--- src/main/java/com/example/demo/HelloApplication.java
+++ src/main/java/com/example/demo/HelloApplication.java
@@ -1,17 +1,37 @@
 package com.example.demo;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
-import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+    public void start(Stage stage) throws Exception {
+        /*
+            A group extends Parent and Parent extends Node
+            here we use this as the Scene graph and give it to construct a scene
+         */
+        Group group = new Group();
+        /*
+            A scene is needed for a stage to display the scene content
+            and a scene need content, so we have that group
+         */
+        Scene scene = new Scene(group);
+        /*
+            We can set different fields of the stage to values we want
+            like adding an icon, changing the width and height, and so on...
+            here an icon is an image, so we have to load it as an image, the starting directory of searching in jfx is resources folder
+         */
+        Image icon = new Image("icons/icon.png");
+        //here we have to get the icons List and add our icon to it
+        stage.getIcons().add(icon);
+        stage.setTitle("I hate saber, rin best girl <3");
+        /*
+            here we assign the scene we created to the stage
+            and make it visible via stage.show() method
+         */
         stage.setScene(scene);
         stage.show();
     }
